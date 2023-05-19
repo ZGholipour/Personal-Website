af23ca17540473f69cc9a57c79971fced926e9fe
ZGholipour <z.gholipour.work@gmail.com>
2023-05-19 07:45:58 +0330

    project updated

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const navToggleIcon = document.querySelector(".nav__toggle-icon");
-const menuItem = document.querySelector(".menu");
-const coverItem = document.querySelector(".cover");
-
-navToggleIcon.addEventListener("click", function () {
-  this.classList.toggle("nav__toggle-icon--open");
-  menuItem.classList.toggle("menu--open");
-  coverItem.classList.toggle("cover--show");
-});
